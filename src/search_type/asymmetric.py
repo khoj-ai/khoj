4d37ace3d60d02448df86ae5de570c452637b35d
Debanjum Singh Solanky <debanjum@gmail.com>
2021-11-17 00:50:28 +0530

    Reduce search results for cross-encoder to re-rank to improve search speed
    Search time on my notes reduced from 14s to 4s. Cross-encoder
    re-ranking step takes majority time, not the cosine similarity search

--- src/search_type/asymmetric.py
+++ src/search_type/asymmetric.py
@@ -24,7 +24,7 @@ def initialize_model():
     "Initialize model for assymetric semantic search. That is, where query smaller than results"
     torch.set_num_threads(4)
     bi_encoder = SentenceTransformer('sentence-transformers/msmarco-MiniLM-L-6-v3')  # The bi-encoder encodes all entries to use for semantic search
-    top_k = 100                                                                      # Number of entries we want to retrieve with the bi-encoder
+    top_k = 30                                                                       # Number of entries we want to retrieve with the bi-encoder
     cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')             # The cross-encoder re-ranks the results to improve quality
     return bi_encoder, cross_encoder, top_k
 
