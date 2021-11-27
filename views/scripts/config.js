fa15d8dcf691fb3a724c2583e3b143e45c4778d4
Saba <narmiabas@gmail.com>
2021-11-27 10:52:54 -0500

    Rename readWriteConfig to just config.js

--- views/scripts/config.js
+++ views/scripts/config.js
@@ -1,8 +1,10 @@
 var showConfig = document.getElementById("show-config");
+var configForm = document.getElementById("config-form");
 showConfig.addEventListener("click", () => {
     fetch("/config")
         .then(response => response.json())
         .then(data => { 
+            configForm.style.display = "block";
             for (let key in data) {
                 console.log('key: ', key);
                 console.log(data[key]);
