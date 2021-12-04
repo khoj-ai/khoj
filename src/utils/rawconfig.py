4d6284b0afc2af72108eed7f61e2e4cd4129c5b6
Saba <narmiabas@gmail.com>
2021-12-04 10:44:13 -0500

    Remove Test suffix from Config models

--- src/utils/rawconfig.py
+++ src/utils/rawconfig.py
@@ -13,18 +13,18 @@ class ConfigBaseModel(BaseModel):
         alias_generator = to_snake_case_from_dash
         allow_population_by_field_name = True
 
-class SearchConfigTest(ConfigBaseModel):
+class SearchConfig(ConfigBaseModel):
     input_files: Optional[List[str]]
     input_filter: Optional[str]
     embeddings_file: Optional[Path]
 
-class TextSearchConfigTest(ConfigBaseModel):
+class TextSearchConfig(ConfigBaseModel):
     compressed_jsonl: Optional[Path]
     input_files: Optional[List[str]]
     input_filter: Optional[str]
     embeddings_file: Optional[Path]
 
-class ImageSearchConfigTest(ConfigBaseModel):
+class ImageSearchConfig(ConfigBaseModel):
     use_xmp_metadata: Optional[str]
     batch_size: Optional[int]
     input_directory: Optional[List[str]]
@@ -32,10 +32,10 @@ class ImageSearchConfigTest(ConfigBaseModel):
     embeddings_file: Optional[Path]
 
 class ContentType(ConfigBaseModel):
-    org: Optional[TextSearchConfigTest]
-    ledger: Optional[TextSearchConfigTest]
-    image: Optional[ImageSearchConfigTest]
-    music: Optional[TextSearchConfigTest]
+    org: Optional[TextSearchConfig]
+    ledger: Optional[TextSearchConfig]
+    image: Optional[ImageSearchConfig]
+    music: Optional[TextSearchConfig]
 
 class AsymmetricConfig(ConfigBaseModel):
     encoder: Optional[str]
@@ -44,7 +44,7 @@ class AsymmetricConfig(ConfigBaseModel):
 class ImageSearchTypeConfig(ConfigBaseModel):
     encoder: Optional[str]
 
-class SearchTypeConfigTest(ConfigBaseModel):
+class SearchTypeConfig(ConfigBaseModel):
     asymmetric: Optional[AsymmetricConfig]
     image: Optional[ImageSearchTypeConfig]
 
@@ -53,10 +53,10 @@ class ProcessorConversationConfig(ConfigBaseModel):
     conversation_logfile: Optional[str]
     conversation_history: Optional[str]
 
-class ProcessorConfigTest(ConfigBaseModel):
+class ProcessorConfig(ConfigBaseModel):
     conversation: Optional[ProcessorConversationConfig]
 
 class FullConfig(ConfigBaseModel):
     content_type: Optional[ContentType]
-    search_type: Optional[SearchTypeConfigTest]
-    processor: Optional[ProcessorConfigTest]
+    search_type: Optional[SearchTypeConfig]
+    processor: Optional[ProcessorConfig]
