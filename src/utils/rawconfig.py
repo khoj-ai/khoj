365ab0c00ec8ab5cf44e759a62d50ec5119f4c38
Saba <narmiabas@gmail.com>
2022-09-14 11:41:44 +0300

    Remove unnecessary panchayat import in rawconfig.py

--- src/utils/rawconfig.py
+++ src/utils/rawconfig.py
@@ -4,7 +4,6 @@ from typing import List, Optional
 
 # External Packages
 from pydantic import BaseModel, validator
-import panchayat
 
 # Internal Packages
 from src.utils.helpers import to_snake_case_from_dash
