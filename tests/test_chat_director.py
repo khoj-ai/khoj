f09bdd515b8a12906c7841f036920972628b3924
Debanjum Singh Solanky <debanjum@gmail.com>
2023-03-16 14:54:56 -0600

    Expect Chat Director can extract relative dates using new Search Actor

--- tests/test_chat_director.py
+++ tests/test_chat_director.py
@@ -179,7 +179,6 @@ def test_answer_requires_current_date_awareness(chat_client):
 
 
 # ----------------------------------------------------------------------------------------------------
-@pytest.mark.xfail(AssertionError, reason="Chat director not capable of answering time aware questions yet")
 @pytest.mark.chatquality
 @freeze_time("2023-04-01")
 def test_answer_requires_date_aware_aggregation_across_provided_notes(chat_client):
@@ -263,7 +262,6 @@ def test_answer_in_chat_history_beyond_lookback_window(chat_client):
 
 
 # ----------------------------------------------------------------------------------------------------
-@pytest.mark.xfail(reason="Chat director not capable of answering this question yet")
 @pytest.mark.chatquality
 def test_answer_requires_multiple_independent_searches(chat_client):
     "Chat director should be able to answer by doing multiple independent searches for required information"
