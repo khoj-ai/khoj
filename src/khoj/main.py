bc4001db4c9ebdb7df2dd55f6acee46a9e20a976
sabaimran <narmiabas@gmail.com>
2023-07-25 17:57:42 -0700

    Try moving pyside imports back down into gui-specific logic

--- src/khoj/main.py
+++ src/khoj/main.py
@@ -18,12 +18,6 @@ import webbrowser
 warnings.filterwarnings("ignore", message=r"snapshot_download.py has been made private", category=FutureWarning)
 warnings.filterwarnings("ignore", message=r"legacy way to download files from the HF hub,", category=FutureWarning)
 
-from PySide6 import QtWidgets
-from PySide6.QtCore import QTimer
-
-# from khoj.interface.desktop.main_window import MainWindow, ServerThread
-# from khoj.interface.desktop.system_tray import create_system_tray
-
 # External Packages
 import uvicorn
 from fastapi import FastAPI
@@ -80,6 +74,9 @@ def run():
         configure_routes(app)
         start_server(app, host=args.host, port=args.port, socket=args.socket)
     else:
+        from PySide6 import QtWidgets
+        from PySide6.QtCore import QTimer
+
         from khoj.interface.desktop.main_window import MainWindow, ServerThread
         from khoj.interface.desktop.system_tray import create_system_tray
 
