860c4389eccc8441a37206d9b95904b3eb6c0996
Ellen7ions <lzz765177255@gmail.com>
2023-08-04 17:18:05 +0800

    Some typos

--- src/khoj/utils/config.py
+++ src/khoj/utils/config.py
@@ -19,7 +19,7 @@ if TYPE_CHECKING:
     from sentence_transformers import CrossEncoder
     from khoj.search_filter.base_filter import BaseFilter
     from khoj.utils.models import BaseEncoder
-    from khoj.utils.rawconfig import ConversationProcessorConfig, Entry, OpenAIProcessorConfig
+    from khoj.utils.rawconfig import ConversationProcessorConfig, Entry, OpenAIProcessorConfig, PageContentConfig
 
 
 class SearchType(str, Enum):
@@ -68,7 +68,7 @@ class ContentIndex:
     org: Optional[TextContent] = None
     markdown: Optional[TextContent] = None
     pdf: Optional[TextContent] = None
-    url: Optional[PageContentConfig] = None
+    url: Optional[TextContent] = None
     github: Optional[TextContent] = None
     notion: Optional[TextContent] = None
     image: Optional[ImageContent] = None
