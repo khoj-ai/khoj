3ff4e19dd218c57af8da05b0b7c0c7349f99bb97
Debanjum Singh Solanky <debanjum@gmail.com>
2023-08-16 22:53:29 -0700

    Release Khoj version 0.11.1

--- src/interface/emacs/khoj.el
+++ src/interface/emacs/khoj.el
@@ -5,7 +5,7 @@
 ;; Author: Debanjum Singh Solanky <debanjum@gmail.com>
 ;; Description: An AI personal assistant for your digital brain
 ;; Keywords: search, chat, org-mode, outlines, markdown, pdf, image
-;; Version: 0.11.0
+;; Version: 0.11.1
 ;; Package-Requires: ((emacs "27.1") (transient "0.3.0") (dash "2.19.1"))
 ;; URL: https://github.com/khoj-ai/khoj/tree/master/src/interface/emacs
 
