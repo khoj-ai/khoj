a7a151325ae6c261472e11e7b93c1dc37c879366
sabaimran <narmiabas@gmail.com>
2023-08-26 14:07:07 -0700

    Update gpt4all tests to use md configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -233,22 +233,25 @@ def client(content_config: ContentConfig, search_config: SearchConfig, processor
 
 @pytest.fixture(scope="function")
 def client_offline_chat(
-    content_config: ContentConfig, search_config: SearchConfig, processor_config_offline_chat: ProcessorConfig
+    md_content_config: ContentConfig, search_config: SearchConfig, processor_config_offline_chat: ProcessorConfig
 ):
-    state.config.content_type = content_config
+    # Initialize app state
+    state.config.content_type = md_content_config
     state.config.search_type = search_config
     state.SearchType = configure_search_types(state.config)
 
-    # These lines help us Mock the Search models for these search types
+    # Index Markdown Content for Search
+    filters = [DateFilter(), WordFilter(), FileFilter()]
     state.search_models.text_search = text_search.initialize_model(search_config.asymmetric)
-    state.search_models.image_search = image_search.initialize_model(search_config.image)
-    state.content_index.org = text_search.setup(
-        OrgToJsonl, content_config.org, state.search_models.text_search.bi_encoder, regenerate=False
-    )
-    state.content_index.image = image_search.setup(
-        content_config.image, state.search_models.image_search, regenerate=False
+    state.content_index.markdown = text_search.setup(
+        MarkdownToJsonl,
+        md_content_config.markdown,
+        state.search_models.text_search.bi_encoder,
+        regenerate=False,
+        filters=filters,
     )
 
+    # Initialize Processor from Config
     state.processor_config = configure_processor(processor_config_offline_chat)
 
     configure_routes(app)
