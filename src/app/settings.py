9f9697920ba82436f3ffd104320a37676ad92251
sabaimran <narmiabas@gmail.com>
2023-11-02 15:40:33 -0700

    Updated ALLOWED_HOSTS for explicit setting

--- src/app/settings.py
+++ src/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*.khoj.dev", "localhost", "127.0.0.1", "[::1]"]
+ALLOWED_HOSTS = [".khoj.dev", "localhost", "127.0.0.1", "[::1]", "beta.khoj.dev"]
 
 # Application definition
 
