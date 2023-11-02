33488d60fe0c5d8fa682e85da067b599d1a1b673
sabaimran <narmiabas@gmail.com>
2023-11-02 15:22:01 -0700

    Allow any subdomain of khoj.dev and localhost in the django settings

--- src/app/settings.py
+++ src/app/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*.khoj.dev", "localhost", "127.0.0.1", "[::1]"]
 
 # Application definition
 
