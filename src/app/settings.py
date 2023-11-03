85b2da30155a9ad55fc0f7d8c115a8c9976fa40b
sabaimran <narmiabas@gmail.com>
2023-11-02 17:26:08 -0700

    Use DJANGO_DEBUG settings dynamically

--- src/app/settings.py
+++ src/app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [".khoj.dev", "localhost", "127.0.0.1", "[::1]", "beta.khoj.dev"]
 
