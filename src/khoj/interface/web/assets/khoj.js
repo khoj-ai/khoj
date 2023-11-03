4fbe8ac6b1571ef3e7b91bbdc512b920f543da0a
Debanjum Singh Solanky <debanjum@gmail.com>
2023-11-02 19:46:12 -0700

    Console log a welcome message on loading web client

--- src/khoj/interface/web/assets/khoj.js
+++ src/khoj/interface/web/assets/khoj.js
@@ -13,3 +13,19 @@ document.addEventListener('click', function(event) {
         menu.classList.remove("show");
     }
 });
+
+console.log(`%c %s`, "font-family:monospace", `
+ __  __     __  __     ______       __        _____      __
+/\\ \\/ /    /\\ \\_\\ \\   /\\  __ \\     /\\ \\      /\\  __ \\   /\\ \\
+\\ \\  _"-.  \\ \\  __ \\  \\ \\ \\/\\ \\   _\\_\\ \\     \\ \\  __ \\  \\ \\ \\
+ \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\ /\\_____\\     \\ \\_\\ \\_\\  \\ \\_\\
+  \\/_/\\/_/   \\/_/\\/_/   \\/_____/ \\/_____/      \\/_/\\/_/   \\/_/
+
+
+Greetings traveller,
+
+I am ✨Khoj✨, your open-source, personal AI copilot.
+
+See my source code at https://github.com/khoj-ai/khoj
+Read my operating manual at https://docs.khoj.dev
+`);
