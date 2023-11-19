e1bf1f0e862c4398d3eec9358d119f26ec44cd8d
Debanjum Singh Solanky <debanjum@gmail.com>
2023-11-18 16:14:35 -0800

    Update default Khoj server URL to Khoj cloud on Emacs, Obsidian clients

--- src/interface/emacs/khoj.el
+++ src/interface/emacs/khoj.el
@@ -63,7 +63,7 @@
 ;; Khoj Static Configuration
 ;; -------------------------
 
-(defcustom khoj-server-url "http://localhost:42110"
+(defcustom khoj-server-url "https://app.khoj.dev"
   "Location of Khoj API server."
   :group 'khoj
   :type 'string)
@@ -94,7 +94,7 @@
   :type 'number)
 
 (defcustom khoj-api-key nil
-  "API Key to Khoj server."
+  "API Key to your Khoj. Default at https://app.khoj.dev/config#clients."
   :group 'khoj
   :type 'string)
 
