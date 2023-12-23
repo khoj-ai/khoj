c50eb8a691cc88b37e3d74001d228d7a668fa9ed
sabaimran <narmiabas@gmail.com>
2023-12-23 11:44:37 +0530

    Fix mypy/pre-commit issues

--- src/khoj/routers/api.py
+++ src/khoj/routers/api.py
@@ -863,6 +863,7 @@ async def extract_references_and_questions(
 
     return compiled_references, inferred_queries, defiltered_query
 
+
 @api.get("/health")
 async def health_check():
     return Response(status_code=200)
