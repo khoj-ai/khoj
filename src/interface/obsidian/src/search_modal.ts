1a46734485ffe04c372c1857989d78c4081c6084
Debanjum Singh Solanky <debanjum@gmail.com>
2024-01-18 10:00:23 +0530

    Fix markdownRenderer arg to allow chat responses in Obsidian plugin
    - Issue: Users with Dataview plugin would have error as its markdown
    post-processor expects the sourcePath to be a string
    
    This prevents Khoj from responding to chat messages in the Obsidian
    chat modal. Search via Obsidian still works but it throws the same
    dataview error
    
    - Fix: Pass a string as sourcePath to markdownRenderer to fix
    failing chat response
    
    Resolves #614, Resolves #606

--- src/interface/obsidian/src/search_modal.ts
+++ src/interface/obsidian/src/search_modal.ts
@@ -124,7 +124,7 @@ export class KhojSearchModal extends SuggestModal<SearchResult> {
         let result_el = el.createEl("div", { cls: 'khoj-result-entry' })
 
         // @ts-ignore
-        MarkdownRenderer.renderMarkdown(snipped_entry + entry_snipped_indicator, result_el, null, null);
+        MarkdownRenderer.renderMarkdown(snipped_entry + entry_snipped_indicator, result_el, result.file, null);
     }
 
     async onChooseSuggestion(result: SearchResult, _: MouseEvent | KeyboardEvent) {
