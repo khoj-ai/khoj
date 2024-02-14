9d4d426512b3fac10c364281409173a11639b80e
sabaimran <narmiabas@gmail.com>
2024-02-14 17:17:50 +0530

    consolidate methods for generating mock chat history

--- tests/test_openai_chat_director.py
+++ tests/test_openai_chat_director.py
@@ -22,7 +22,7 @@ if api_key is None:
 
 # Helpers
 # ----------------------------------------------------------------------------------------------------
-def populate_chat_history(message_list, user=None):
+def generate_history(message_list):
     # Generate conversation logs
     conversation_log = {"chat": []}
     for user_message, gpt_message, context in message_list:
@@ -31,6 +31,12 @@ def populate_chat_history(message_list, user=None):
             gpt_message,
             {"context": context, "intent": {"query": user_message, "inferred-queries": f'["{user_message}"]'}},
         )
+    return conversation_log
+
+
+def populate_chat_history(message_list, user):
+    # Generate conversation logs
+    conversation_log = generate_history(message_list)
 
     # Update Conversation Metadata Logs in Database
     ConversationFactory(user=user, conversation_log=conversation_log)
@@ -491,10 +497,11 @@ async def test_get_correct_tools_with_chat_history(chat_client):
         (
             "Let's talk about the current events around the world.",
             "Sure, let's discuss the current events. What would you like to know?",
+            [],
         ),
-        ("What's up in New York City?", "A Pride parade has recently been held in New York City, on July 31st."),
+        ("What's up in New York City?", "A Pride parade has recently been held in New York City, on July 31st.", []),
     ]
-    chat_history = populate_chat_history(chat_log)
+    chat_history = generate_history(chat_log)
 
     # Act
     tools = await aget_relevant_information_sources(user_query, chat_history)
@@ -502,15 +509,3 @@ async def test_get_correct_tools_with_chat_history(chat_client):
     # Assert
     tools = [tool.value for tool in tools]
     assert tools == ["online"]
-
-
-def populate_chat_history(message_list):
-    # Generate conversation logs
-    conversation_log = {"chat": []}
-    for user_message, gpt_message in message_list:
-        conversation_log["chat"] += message_to_log(
-            user_message,
-            gpt_message,
-            {"context": [], "intent": {"query": user_message, "inferred-queries": f'["{user_message}"]'}},
-        )
-    return conversation_log
