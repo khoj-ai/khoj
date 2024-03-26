4d49430b0bc1e950a545108e3598b2a28bd34b36
sabaimran <narmiabas@gmail.com>
2024-03-26 16:17:39 +0530

    Standardize to use personality over tuning in agent nomenclature

--- src/khoj/routers/api_agents.py
+++ src/khoj/routers/api_agents.py
@@ -30,7 +30,7 @@ async def all_agents(
                 "slug": agent.slug,
                 "avatar": agent.avatar,
                 "name": agent.name,
-                "tuning": agent.tuning,
+                "personality": agent.personality,
                 "public": agent.public,
                 "creator": agent.creator.username if agent.creator else None,
                 "managed_by_admin": agent.managed_by_admin,
