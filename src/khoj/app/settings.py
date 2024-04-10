3fe94a67b0d03bb414533a1753c3b748769f5151
sabaimran <65192171+sabaimran@users.noreply.github.com>
2024-04-10 07:27:33 -0700

    Send welcome emails when a new user signs up (#691)
    * Don't trigger any re-indexing on server initailization
    
    * Integrate Resend to send welcome emails when a new user signs up
    
    - Only send if this is the first time they've signed in
    - Configure welcome email with basic styling, as more complex designs don't work and style tag did not work

--- src/khoj/app/settings.py
+++ src/khoj/app/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [BASE_DIR / "interface/web"]
+STATICFILES_DIRS = [BASE_DIR / "interface/web", BASE_DIR / "interface/email"]
 STATIC_URL = "/static/"
 
 # Default primary key field type
--- src/khoj/routers/api.py
+++ src/khoj/routers/api.py
@@ -182,7 +182,7 @@ def update(
         logger.warning(error_msg)
         raise HTTPException(status_code=500, detail=error_msg)
     try:
-        initialize_content(regenerate=force, search_type=t, init=False, user=user)
+        initialize_content(regenerate=force, search_type=t, user=user)
     except Exception as e:
         error_msg = f"🚨 Failed to update server via API: {e}"
         logger.error(error_msg, exc_info=True)
