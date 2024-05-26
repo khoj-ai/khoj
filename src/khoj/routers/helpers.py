86283fec68c20752da4c4684052c6afaa1c4ba3c
sabaimran <narmiabas@gmail.com>
2024-05-26 18:46:58 +0530

    Remove response_type argument from the anthropic send to model wrapper function

--- src/khoj/routers/helpers.py
+++ src/khoj/routers/helpers.py
@@ -585,7 +585,6 @@ def send_message_to_model_wrapper_sync(
             messages=truncated_messages,
             api_key=api_key,
             model=chat_model,
-            response_type=response_type,
         )
     else:
         raise HTTPException(status_code=500, detail="Invalid conversation config")
