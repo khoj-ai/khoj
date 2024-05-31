fd07abbfc8794a9af9f66689e124d314c2deda8d
sabaimran <narmiabas@gmail.com>
2024-05-31 15:39:15 +0530

    Decrease the life of one connection

--- src/khoj/app/settings.py
+++ src/khoj/app/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "NAME": os.getenv("POSTGRES_DB", "khoj"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "CONN_MAX_AGE": 240,
+        "CONN_MAX_AGE": 200,
     }
 }
 
