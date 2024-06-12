985dd1a06da38737bdc6baf38036ff5665edafef
sabaimran <narmiabas@gmail.com>
2024-06-12 14:32:30 +0530

    Remove unused api agent get method

--- src/khoj/routers/api_agents.py
+++ src/khoj/routers/api_agents.py
@@ -41,28 +41,3 @@ async def all_agents(
     agents_packet.sort(key=lambda x: x["name"])
     agents_packet.sort(key=lambda x: x["slug"] == "khoj", reverse=True)
     return Response(content=json.dumps(agents_packet), media_type="application/json", status_code=200)
-
-
-@api_agents.get("/{slug}", response_class=Response)
-async def get_agent(
-    request: Request,
-    slug: str,
-    common: CommonQueryParams,
-) -> Response:
-    user: KhojUser = request.user.object if request.user.is_authenticated else None
-    agent = await AgentAdapters.get_agent_by_slug(slug, user)
-    if agent is None:
-        return Response(
-            content=json.dumps({"error": "Agent not found"}), media_type="application/json", status_code=404
-        )
-
-    agent_packet = {
-        "slug": agent.slug,
-        "avatar": agent.avatar,
-        "name": agent.name,
-        "personality": agent.personality,
-        "public": agent.public,
-        "creator": agent.creator.username if agent.creator else None,
-        "managed_by_admin": agent.managed_by_admin,
-    }
-    return Response(content=json.dumps(agent_packet), media_type="application/json", status_code=200)
