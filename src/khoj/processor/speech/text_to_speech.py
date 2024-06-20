61115843ee760d7e0edad54dac8e46ffca54f34a
sabaimran <narmiabas@gmail.com>
2024-06-20 14:16:57 +0530

    s/XI_/ELEVEN_LABS

--- src/khoj/processor/speech/text_to_speech.py
+++ src/khoj/processor/speech/text_to_speech.py
@@ -7,7 +7,7 @@ from markdown_it import MarkdownIt
 
 # Define constants for the script
 CHUNK_SIZE = 1024  # Size of chunks to read/write at a time
-XI_API_KEY = os.getenv("ELEVEN_LABS_API_KEY", None)  # Your API key for authentication
+ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY", None)  # Your API key for authentication
 VOICE_ID = "29vD33N1CtxCmqQRPOHJ"  # ID of the voice model to use. MALE.
 ELEVEN_API_URL = "https://api.elevenlabs.io/v1/text-to-speech"  # Base URL for the Text-to-Speech API
 
@@ -15,7 +15,7 @@ markdown_renderer = MarkdownIt()
 
 
 def is_eleven_labs_enabled():
-    return XI_API_KEY is not None
+    return ELEVEN_LABS_API_KEY is not None
 
 
 def generate_text_to_speech(
@@ -33,7 +33,7 @@ def generate_text_to_speech(
     tts_url = f"{ELEVEN_API_URL}/{voice_id}/stream"
 
     # Set up headers for the API request, including the API key for authentication
-    headers = {"Accept": "application/json", "xi-api-key": XI_API_KEY}
+    headers = {"Accept": "application/json", "xi-api-key": ELEVEN_LABS_API_KEY}
 
     # Set up the data payload for the API request, including the text and voice settings
     data = {
