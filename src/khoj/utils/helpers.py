8eccd8a5e4321cf0602c4d5b77cf3cf3ecc550f1
Raghav Tirumale <62105787+MythicalCow@users.noreply.github.com>
2024-07-01 09:00:00 -0400

    Support Indexing Images via OCR (#823)
    - Added support for uploading .jpeg, .jpg, and .png files to Khoj from Web, Desktop app
    - Updating indexer to generate raw text and entries using RapidOCR
    - Details
      * added support for indexing images via ocr
      * fixed pyproject.toml
      * Update src/khoj/processor/content/images/image_to_entries.py
         Co-authored-by: Debanjum <debanjum@gmail.com>
      * Update src/khoj/processor/content/images/image_to_entries.py
         Co-authored-by: Debanjum <debanjum@gmail.com>
      * removed redudant try except blocks
      * updated desktop js file to support image formats
      * added tests for jpg and png
      * Fix processing for image to entries files
      * Update unit tests with working image indexer
      * Change png test from version verificaition to open-cv verification
    
    ---------
    
    Co-authored-by: Debanjum <debanjum@gmail.com>
    Co-authored-by: sabaimran <narmiabas@gmail.com>

--- src/khoj/utils/helpers.py
+++ src/khoj/utils/helpers.py
@@ -118,9 +118,9 @@ def get_file_type(file_type: str, file_content: bytes) -> tuple[str, str]:
     elif file_type in ["application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"]:
         return "docx", encoding
     elif file_type in ["image/jpeg"]:
-        return "jpeg", encoding
+        return "image", encoding
     elif file_type in ["image/png"]:
-        return "png", encoding
+        return "image", encoding
     elif content_group in ["code", "text"]:
         return "plaintext", encoding
     else:
