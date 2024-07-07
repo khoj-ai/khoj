0eb000c3eab478ccb42f4896491ab96dc0249d7e
sabaimran <narmiabas@gmail.com>
2024-07-07 16:11:28 +0530

    Add health checks for the django ORM

--- src/khoj/app/settings.py
+++ src/khoj/app/settings.py
@@ -122,6 +122,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB", "khoj"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
         "CONN_MAX_AGE": 0,
+        "CONN_HEALTH_CHECKS": True,
     }
 }
 
