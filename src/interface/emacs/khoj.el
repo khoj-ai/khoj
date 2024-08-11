d951e369457654ace53ea1305eff96bc995011e0
Debanjum Singh Solanky <debanjum@gmail.com>
2024-08-11 15:51:58 +0530

    Update khoj.el package description, it had gone stale

--- src/interface/emacs/khoj.el
+++ src/interface/emacs/khoj.el
@@ -1,4 +1,4 @@
-;;; khoj.el --- AI copilot for your Second Brain -*- lexical-binding: t -*-
+;;; khoj.el --- Your Second Brain -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2021-2023 Khoj Inc.
 
@@ -29,19 +29,20 @@
 
 ;;; Commentary:
 
-;; Create an AI copilot to your `org-mode', `markdown' notes,
-;; PDFs and images. The copilot exposes 2 modes, search and chat:
+;; Bootstrap your Second Brain from your `org-mode', `markdown' notes,
+;; PDFs and images. Khoj exposes 2 modes, search and chat:
 ;;
 ;; Chat provides faster answers, iterative discovery and assisted
-;; creativity. It requires your OpenAI API key to access GPT models
+;; creativity.
 ;;
-;; Search allows natural language, incremental and local search.
-;; It relies on AI models that run locally on your machine.
+;; Search allows natural language, incremental search.
 ;;
 ;; Quickstart
 ;; -------------
 ;; 1. Install khoj.el from MELPA Stable
 ;;    (use-package khoj :pin melpa-stable :bind ("C-c s" . 'khoj))
+;; 2. Set API key from https://app.khoj.dev/settings#clients (if not self-hosting)
+;;    (setq khoj-api-key "YOUR_KHOJ_API_KEY")
 ;; 2. Start khoj from Emacs
 ;;    C-c s or M-x khoj
 ;;
