4eeee0a59352a01c849e53bf45530414539ce962
MythicalCow <raghav.tirumale@gmail.com>
2024-08-16 14:18:16 -0400

    tightening CSP

--- src/interface/web/app/layout.tsx
+++ src/interface/web/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
                 content="default-src 'self' https://assets.khoj.dev;
                        media-src * blob:;
                        script-src 'self' https://assets.khoj.dev 'unsafe-inline' 'unsafe-eval';
-                       connect-src 'self' data: blob: https://ipapi.co/json ws://localhost:42110;
+                       connect-src 'self' blob: https://ipapi.co/json ws://localhost:42110;
                        style-src 'self' https://assets.khoj.dev 'unsafe-inline' https://fonts.googleapis.com;
                        img-src 'self' data: blob: https://*.khoj.dev https://*.googleusercontent.com https://*.google.com/ https://*.gstatic.com;
                        font-src 'self' https://assets.khoj.dev https://fonts.gstatic.com;
