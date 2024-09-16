ac8a762735649193fe85ed537fa66caa38023424
sabaimran <narmiabas@gmail.com>
2024-09-15 21:45:37 -0700

    Add a unique_id field to the conversation object
    - This helps us keep track of the unique identity of the conversation without expose the internal id
    - Create three staged migrations in order to first add the field, then add unique values to pre-fill, and then set the unique constraint. Without this, it tries to initialize all the existing conversations with the same ID.

--- src/khoj/database/models/__init__.py
+++ src/khoj/database/models/__init__.py
@@ -350,6 +350,7 @@ class Conversation(BaseModel):
     title = models.CharField(max_length=200, default=None, null=True, blank=True)
     agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, default=None, null=True, blank=True)
     file_filters = models.JSONField(default=list)
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
 
 class PublicConversation(BaseModel):
