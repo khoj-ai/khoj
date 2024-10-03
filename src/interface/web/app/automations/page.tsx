e217cb5840af509f8ca7d3932c7f401ee9b5ef44
Debanjum Singh Solanky <debanjum@gmail.com>
2024-10-03 16:35:18 -0700

    Suggest notification type automation on Automation page of web app

--- src/interface/web/app/automations/page.tsx
+++ src/interface/web/app/automations/page.tsx
@@ -203,6 +203,15 @@ const suggestedAutomationsMetadata: AutomationsData[] = [
         scheduling_request: "",
     },
     {
+        subject: "Market Crash Notification",
+        query_to_run: "Notify me if the stock market fell by more than 5% today.",
+        schedule: "5PM every evening",
+        next: "Next run at 5PM today",
+        crontime: "0 17 * * *",
+        id: timestamp + 5,
+        scheduling_request: "",
+    },
+    {
         subject: "Round-up of research papers about AI in healthcare",
         query_to_run:
             "Summarize the top 3 research papers about AI in healthcare that were published in the last week. Include links to the full papers.",
