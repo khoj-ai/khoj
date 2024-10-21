32b7ffc15bff78e9cdb2c94a74c76bcdaf399def
sabaimran <narmiabas@gmail.com>
2024-10-21 10:07:56 -0700

    Add some really basic schema validation on the client side

--- src/interface/web/app/components/excalidraw/excalidrawWrapper.tsx
+++ src/interface/web/app/components/excalidraw/excalidrawWrapper.tsx
@@ -90,8 +90,65 @@ export default function ExcalidrawWrapper(props: ExcalidrawWrapperProps) {
     const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
     const [expanded, setExpanded] = useState<boolean>(false);
 
+    const isValidExcalidrawElement = (element: ExcalidrawElementSkeleton): boolean => {
+        return (
+            element.x !== undefined &&
+            element.y !== undefined &&
+            element.id !== undefined &&
+            element.type !== undefined
+        );
+    };
+
     useEffect(() => {
-        const elements = convertToExcalidrawElements(props.data);
+        // Do some basic validation
+        const basicValidSkeletons: ExcalidrawElementSkeleton[] = [];
+
+        for (const element of props.data) {
+            if (isValidExcalidrawElement(element as ExcalidrawElementSkeleton)) {
+                basicValidSkeletons.push(element as ExcalidrawElementSkeleton);
+            }
+        }
+
+        const validSkeletons: ExcalidrawElementSkeleton[] = [];
+        for (const element of basicValidSkeletons) {
+            if (element.type === "frame") {
+                continue;
+            }
+            if (element.type === "arrow" || element.type === "line") {
+                const start = basicValidSkeletons.find((child) => child.id === element.start?.id);
+                const end = basicValidSkeletons.find((child) => child.id === element.end?.id);
+                if (start && end) {
+                    validSkeletons.push(element);
+                }
+            } else {
+                validSkeletons.push(element);
+            }
+        }
+
+        for (const element of basicValidSkeletons) {
+            if (element.type === "frame") {
+                const children = element.children?.map((childId) => {
+                    return validSkeletons.find((child) => child.id === childId);
+                });
+                // Get the valid children, filter out any undefined values
+                const validChildrenIds: readonly string[] = children
+                    ?.map((child) => child?.id)
+                    .filter((id) => id !== undefined) as string[];
+
+                if (validChildrenIds === undefined || validChildrenIds.length === 0) {
+                    continue;
+                }
+
+                validSkeletons.push({
+                    ...element,
+                    children: validChildrenIds,
+                });
+            }
+        }
+
+        console.log("Valid skeletons", validSkeletons);
+
+        const elements = convertToExcalidrawElements(validSkeletons);
         setExcalidrawElements(elements);
     }, []);
 
@@ -138,6 +195,10 @@ export default function ExcalidrawWrapper(props: ExcalidrawWrapperProps) {
                             elements: excalidrawElements,
                             appState: { zenModeEnabled: true },
                             scrollToContent: true,
+                        }}
+                        validateEmbeddable={true}
+                        renderTopRightUI={(isMobile, appState) => {
+                            return <></>;
                         }}
                         excalidrawAPI={(api) => setExcalidrawAPI(api)}
                         onChange={onExcalidrawChange}
