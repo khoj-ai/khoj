3e5b5ec122e4afe0d70ea7e0b6f1d1212c09a894
Debanjum <debanjum@gmail.com>
2024-10-26 10:41:42 -0700

    Encourage model to read webpages more often after online search
    Previously model would rarely read webpages after webpage search. Need
    the model to webpages more regularly for deeper research and to stop
    getting stuck in repetitive online search loops

--- src/khoj/utils/helpers.py
+++ src/khoj/utils/helpers.py
@@ -357,7 +357,7 @@ tool_descriptions_for_llm = {
 function_calling_description_for_llm = {
     ConversationCommand.Notes: "To search the user's personal knowledge base. Especially helpful if the question expects context from the user's notes or documents.",
     ConversationCommand.Online: "To search the internet for the latest, up-to-date information.",
-    ConversationCommand.Webpage: "To read a webpage url for detailed information from the internet.",
+    ConversationCommand.Webpage: "To read a webpage for more detailed research from the internet. Usually used when you have webpage links to refer to.",
     ConversationCommand.Code: "To run Python code in a Pyodide sandbox with no network access. Helpful when need to parse information, run complex calculations, create documents and charts for user. Matplotlib, bs4, pandas, numpy, etc. are available.",
 }
 
