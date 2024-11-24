6a39651ad3974272daf5865586af98c5ff091355
Debanjum <debanjum@gmail.com>
2024-11-24 15:02:14 -0800

    Standardize loading fonts locally across pages on web app

--- src/interface/web/app/chat/layout.tsx
+++ src/interface/web/app/chat/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
+import { noto_sans, noto_sans_arabic } from "@/app/fonts";
 import "../globals.css";
-
-const inter = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Khoj AI - Chat",
@@ -34,7 +32,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${noto_sans.variable} ${noto_sans_arabic.variable}`}>
             <meta
                 httpEquiv="Content-Security-Policy"
                 content="default-src 'self' https://assets.khoj.dev;
@@ -47,7 +45,7 @@ export default function RootLayout({
                        child-src 'none';
                        object-src 'none';"
             ></meta>
-            <body className={inter.className}>
+            <body>
                 {children}
                 <script
                     dangerouslySetInnerHTML={{
