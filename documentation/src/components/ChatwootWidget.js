47a087c73b4f6fa291fd5a825750095893d9463b
sabaimran <narmiabas@gmail.com>
2024-12-08 17:16:20 -0800

    Fix chatwoot import issue by checking whether we're in an execution environment before loading the script

--- documentation/src/components/ChatwootWidget.js
+++ documentation/src/components/ChatwootWidget.js
@@ -1,14 +1,19 @@
-(function (d, t) {
-  var BASE_URL = "https://app.chatwoot.com";
-  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-  g.src = BASE_URL + "/packs/js/sdk.js";
-  g.defer = true;
-  g.async = true;
-  s.parentNode.insertBefore(g, s);
-  g.onload = function () {
-    window.chatwootSDK.run({
-      websiteToken: 'cFxvnLSjfE2UF4UUiPCA5NsF',
-      baseUrl: BASE_URL
-    })
-  }
-})(document, 'script');
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+// Only execute on client-side
+if (ExecutionEnvironment.canUseDOM) {
+  (function (d, t) {
+    var BASE_URL = "https://app.chatwoot.com";
+    var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    g.src = BASE_URL + "/packs/js/sdk.js";
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g, s);
+    g.onload = function () {
+      window.chatwootSDK.run({
+        websiteToken: 'cFxvnLSjfE2UF4UUiPCA5NsF',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document, 'script');
+}
